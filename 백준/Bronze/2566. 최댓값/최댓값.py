88127623b0c6f8f8eb58160ec0e55ab202914903
SONG JU HEE <80883709+songzuhee@users.noreply.github.com>
2024-04-16 13:55:53 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+arr = [list(map(int, input().split())) for _ in range(9)]
+
+max_num = 0
+max_col, max_row = 0, 0
+
+for i in range(9):
+    for j in range(9):
+        if max_num <=  arr[i][j]:
+            max_num = arr[i][j]
+            max_row = i + 1
+            max_col = j + 1
+print(max_num)
+print(max_row, max_col)
