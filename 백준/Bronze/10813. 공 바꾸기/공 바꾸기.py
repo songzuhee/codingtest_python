4cf7779eced6f0d4b1d453c54921cf5dc6e625cf
SONG JU HEE <80883709+songzuhee@users.noreply.github.com>
2024-04-29 14:20:53 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = list(range(1, n+1))
+tmp = 0
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    tmp = arr[i-1]
+    arr[i-1] = arr[j-1]
+    arr[j-1] = tmp
+print(*arr)
