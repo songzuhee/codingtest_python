30e508e1d0bb6ece82bbe41fb277546ca0164199
SONG JU HEE <80883709+songzuhee@users.noreply.github.com>
2024-05-13 14:16:05 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+def gcd(n, m):
+    while m > 0:
+        n, m = m, n%m
+    return n
+
+def lcm(n, m):
+    return (n*m) // gcd(n, m)
+
+print(gcd(n, m))
+print(lcm(n, m))
