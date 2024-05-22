97d7e7f9f440b6f3a5a00057e16cc685dbc189b2
SONG JU HEE <80883709+songzuhee@users.noreply.github.com>
2024-05-22 19:56:50 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+words = []
+for i in range(5):
+    word = input().rstrip()
+    words.append(word)
+for i in range(15):
+    for j in range(5):
+        if i < len(words[j]):
+            print(words[j][i], end="")
