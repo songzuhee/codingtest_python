8ce5b0220b2e4ae4307dfc0b5b70fa355c930d62
SONG JU HEE <80883709+songzuhee@users.noreply.github.com>
2024-07-01 14:59:48 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 228 ms, Memory: 41116 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -5,24 +5,13 @@ input = sys.stdin.readline
 # 5 4 3 2 1
 # 3, 5 ->  3 + 2 + 1 = 6
 
-def prefix_sum(arr):
-    #누적합을 담을 배열 prefixSum
-    prefixSum = [0] * len(arr)
-    prefixSum[0] = arr[0]
-
-    for i in range(1, len(arr)):
-        prefixSum[i] = prefixSum[i-1] + arr[i]
-    return prefixSum
-
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-prefixSum = prefix_sum(arr)
+prefixSum = [0] * (len(arr)+1)
+for i in range(1, len(arr) +1):
+    prefixSum[i] = prefixSum[i-1]+arr[i-1]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    if a == 1:
-        result = prefixSum[b - 1]
-    else:
-        result = prefixSum[b - 1] - prefixSum[a - 2]
-    print(result)
+    print(prefixSum[b] - prefixSum[a-1])
